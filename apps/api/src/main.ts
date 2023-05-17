db266f6f62c00411ee82238a418fd3b4607a5f11
Damjana Baric <dbaric@codeframe.ch>
2023-05-18 00:51:20 +0200

    Added exception filter handler

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,17 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
-// TODO set up swagger
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { PrismaClientExceptionFilter } from './exceptions/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
