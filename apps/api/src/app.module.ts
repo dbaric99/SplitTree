929800e38e62e94b9c6fd326e46307d67ee88abb
Damjana Baric <dbaric@codeframe.ch>
2023-05-18 00:14:45 +0200

    Import modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PrismaModule } from './modules/prisma.module';
+import { AdminsModule } from './modules/admins.module';
+import { CompaniesModule } from './modules/companies.module';
+import { CoursesModule } from './modules/courses.module';
+import { EmployeesModule } from './modules/employees.module';
 
 @Module({
   imports: [
@@ -11,6 +15,10 @@ import { PrismaModule } from './modules/prisma.module';
       rootPath: join(__dirname, '../..', 'client', 'dist'),
     }),
     PrismaModule,
+    AdminsModule,
+    CompaniesModule,
+    CoursesModule,
+    EmployeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
